d3a83ed156b26e262291dbbdc4cc662b404ec213
Jonas-Petrauskas <tiesiogbicas@gmail.com>
2021-11-02 18:42:05 +0100

    ApiClient finished

--- client/src/Services/ApiClient.js
+++ client/src/Services/ApiClient.js
@@ -1,1 +1,29 @@
 const {REACT_APP_API_KEY} = process.env;
+const API_BARCELONA = `https://api.openweathermap.org/data/2.5/weather?q=Barcelona&units=metric&appid=${REACT_APP_API_KEY}`;
+const API_LONDON = `https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=${REACT_APP_API_KEY}`;
+
+export const getBarcelonaWeather = () => fetchRequestBarcelona();
+export const getLondonWeather = () => fetchRequestLondon();
+export const searchCity = () => fetchRequestCity();
+
+const fetchRequestBarcelona = () => {
+  return fetch(API_BARCELONA)
+    .then((res) => (res.status <= 400 ? res : res))
+    .then((res) => res.json())
+    .catch((err) => {
+      console.log(`${err.message} while fetching /${API_BARCELONA}`);
+    });
+};
+
+const fetchRequestLondon = () => {
+  return fetch(API_LONDON)
+    .then((res) => (res.status <= 400 ? res : res))
+    .then((res) => res.json())
+    .catch((err) => {
+      console.log(`${err.message} while fetching /${API_LONDON}`);
+    });
+};
+
+const fetchRequestCity = () => {
+  return;
+};
