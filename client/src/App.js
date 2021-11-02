9b8460d7ce7dae984637d8a09dee7686e2457e86
Jonas-Petrauskas <tiesiogbicas@gmail.com>
2021-11-02 18:48:43 +0100

    adding state

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
-import './scss/App.css';
+import "./scss/App.css";
+import React, { useState, useEffect } from "react";
+
+import * as ApiClient from "./Services/ApiClient";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  // Search query
+  const [weather, setWeather] = useState({});
+  const [query, setQuery] = useState("");
+  // Getting weather for Barcelona
+  const [weatherBarcelona, setWeatherBarcelona] = useState(null);
+  const [cityBarcelona, setCityBarcelona] = useState("");
+  const [temperatureBarcelona, setTemperatureBarcelona] = useState(0);
+  const [iconBarcelona, setIconBarcelona] = useState({});
+  // Getting weather for London
+  const [weatherLondon, setWeatherLondon] = useState(null);
+  const [cityLondon, setCityLondon] = useState("");
+  const [temperatureLondon, setTemperatureLondon] = useState(0);
+  const [iconLondon, setIconLondon] = useState({});
+  // Handring button state
+  const [showBarcelone, setShowBarcelona] = useState(false);
+  const [showLondon, setShowLondon] = useState(false);
+
+  return <div className="App"></div>;
 }
 
 export default App;
